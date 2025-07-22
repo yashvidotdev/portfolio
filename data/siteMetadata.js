7ebcf55f01940dabae3c6884533ade8ff8b0e7ec
Yashvi Shah <yashu.shah28@gmail.com>
2025-07-22 07:06:52 +0530

    update site metadata

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -21,7 +21,7 @@ const siteMetadata = {
     plausibleDataDomain: '', // e.g. tailwind-nextjs-starter-blog.vercel.app
     simpleAnalytics: false, // true or false
     umamiWebsiteId: '', // e.g. 123e4567-e89b-12d3-a456-426614174000
-    googleAnalyticsId: 'G-4ZB6HQZ5E2', // e.g. UA-000000-2 or G-XXXXXXX
+    googleAnalyticsId: '', // e.g. UA-000000-2 or G-XXXXXXX
   },
   newsletter: {
     provider: 'emailOctopus',
